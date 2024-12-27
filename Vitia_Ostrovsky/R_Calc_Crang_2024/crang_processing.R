6c08253d15e42f7a4a2ddfd1d5e0df3d5abeb514
OstrovskiyVictor <fokker4119@gmail.com>
2024-12-27 14:31:06 +0300

    Витя создал папку

---
+++ Vitia_Ostrovsky/R_Calc_Crang_2024/crang_processing.R
@@ -0,0 +1,53 @@
+library(readxl)
+library(vegan)
+library(dplyr)
+
+Crang_2024 <- read_excel("Data/Crang_2024.xlsx", na = "NA")
+
+str(Crang_2024)
+
+
+
+
+Crang_2024 %>%
+  select(-c( ID, W, L_Car)) %>% 
+  group_by(Site, Sample) %>% 
+  summarise_all(.funs = "mean") ->
+  crang
+
+
+ord_crang <- metaMDS(crang[ ,-c(1:2) ], distance = "bray")
+
+plot(ord_crang, type = "t")
+
+
+library(ggplot2)
+
+mds_points <- data.frame(scores(ord_crang)$sites)
+
+mds_points$Area <- crang$Site
+
+ggplot(mds_points, aes(NMDS1, NMDS2, color=Area)) +
+  geom_point(size = 4)
+
+
+
+
+
+
+
+crang <- Crang_2024 %>%
+  select(-c(ID, W, L_Car, Lat, Lon)) %>%
+  group_by(Area, Sample) %>%
+  summarise(across(everything(), mean), .groups = 'drop')
+
+ord_crang <- metaMDS(crang[,-c(1:2)], distance = "bray")
+
+plot(ord_crang, type = "t")
+
+mds_points <- data.frame(scores(ord_crang)$sites)
+
+mds_points$Area <-crang$Area
+ggplot(mds_points, aes(  NMDS1, NMDS2,t, color = Area)) +
+  geom_point(size = 4)
+
