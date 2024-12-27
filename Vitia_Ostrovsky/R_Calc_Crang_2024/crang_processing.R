223341acf1282516ca0c4010546d5fd7370e2fb5
OstrovskiyVictor <fokker4119@gmail.com>
2024-12-27 16:30:04 +0300

    Update crang_processing.R

--- Vitia_Ostrovsky/R_Calc_Crang_2024/crang_processing.R
+++ Vitia_Ostrovsky/R_Calc_Crang_2024/crang_processing.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(vegan)
 library(dplyr)
+library(ggplot2)
 
 Crang_2024 <- read_excel("Data/Crang_2024.xlsx", na = "NA")
 
@@ -21,7 +22,7 @@ ord_crang <- metaMDS(crang[ ,-c(1:2) ], distance = "bray")
 plot(ord_crang, type = "t")
 
 
-library(ggplot2)
+
 
 mds_points <- data.frame(scores(ord_crang)$sites)
 
@@ -53,4 +54,63 @@ mds_points_comm <- data.frame(scores(ord_comm)$sites)
 mds_points_comm$Area <-comm$Area
 ggplot(mds_points_comm, aes(  NMDS1, NMDS2,t, color = Area)) +
   geom_point(size = 4)
+################
 
+crang %>% 
+  select(Area,Site,Empty) ->
+  diverse
+  
+
+
+crang %>% 
+  ungroup() %>% 
+  select(-c(Area,Site,Empty)) %>% 
+  specnumber() ->
+  diverse$Spec_Num
+
+
+crang %>% 
+  ungroup() %>% 
+  select(-c(Area,Site,Empty)) %>% 
+  diversity() ->
+  diverse$H_crang
+
+
+comm %>% 
+  ungroup() %>% 
+  select(-c(Area, Site)) %>% 
+  diversity()->
+  diverse$H_com
+
+ggplot(diverse, aes(x = H_com, y = Spec_Num))+
+  geom_point()+ 
+  geom_smooth(method = "lm")
+
+
+ggplot(diverse, aes(x = H_com, y = Empty))+
+  geom_point()+ 
+  geom_smooth(method = "lm")
+
+ggplot(diverse, aes(x = H_com, y = H_crang))+
+  geom_point(aes(color = Area))+
+  geom_smooth(method = "lm")
+
+
+
+comm%>%
+  ungroup() %>% 
+  select(-c(Area, Site)) %>% 
+  rowSums() ->
+  diverse$comm_total_N
+
+ggplot(diverse, aes(x = comm_total_N, y = H_crang))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+ggplot(diverse, aes(x = comm_total_N, y = Spec_Num))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+
+
+round(vegdist(comm[,-c(1:2)]), 2)
