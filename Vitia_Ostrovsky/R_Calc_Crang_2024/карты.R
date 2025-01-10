ce6dcb5eeddba6156a6e55f7314736ed6f2400b7
OstrovskiyVictor <fokker4119@gmail.com>
2025-01-10 20:03:21 +0300

    Витя 10.01.25

--- Vitia_Ostrovsky/R_Calc_Crang_2024/карты.R
+++ Vitia_Ostrovsky/R_Calc_Crang_2024/карты.R
@@ -1,6 +1,6 @@
 library(readxl)
-outline <- read_excel("Data/Crang 2022 full.xlsx", sheet = "Outline",  na = "NA")
-sampl <- read_excel("Data/Crang 2022 full.xlsx" , sheet = "Coordinates",  na = "NA")
+outline <- read_excel("Data/Crang_2024.xlsx", sheet = "Outline_s",  na = "NA")
+sampl <- read_excel("Data/Crang_2024.xlsx" , sheet = "Coordinates_s",  na = "NA")
   
 Pl_map <-
   ggplot(outline, aes(x = Lon, y = Lat)) + 
@@ -11,21 +11,5 @@ Pl_map <-
     
 Pl_map + geom_point(data = sampl) 
     
-    
-    Outline_N <- read_excel("Data/Crang_2024.xlsx", sheet="Outline", na ="NA")
-    
-    samples_N <- read_excel("Data/Crang_2024.xlsx" , sheet = "Nor")
-    Samples_Nord <- read_excel("Data/Crang_2024.xlsx", sheet = "Nord")
-    Pl_map_N <-
-    ggplot(Outline_N, aes(x = Lon, y = Lat)) + 
-      geom_path(aes(linetype = Type)) + 
-      guides(linetype = "none") + 
-      scale_linetype_manual(values = c(2,1, 3)) +
-      theme_bw() 
-    
-    Pl_map_N + geom_point(data = samples_N)
-   
-    
-    
-    Pl_map_N + geom_point(data = Samples_Nord) 
+ 
     
