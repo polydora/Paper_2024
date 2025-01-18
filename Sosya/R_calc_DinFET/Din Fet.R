02463eaa66121cff2b2015e06f128caee6910667
Vadim Khaitov <polydora@rambler.ru>
2025-01-18 18:45:56 +0300

    Merge branch 'main' of https://github.com/polydora/Paper_2024

--- Sosya/R_calc_DinFET/Din Fet.R
+++ Sosya/R_calc_DinFET/Din Fet.R
@@ -20,9 +20,29 @@ ggplot(data = dinfet, aes(x = (Branch), y = log(Force), color = F_Sp)) +
   facet_wrap(~Morphotype) + 
   geom_smooth(method = "lm")
 
-Mod <- lm(log(Force) ~ Branch * Morphotype * F_Sp, data = dinfet)
 
-drop1(Mod, test = "F")
+
+ggplot(data = dinfet, aes(x = (Branch), y = log(Mussel_Weight), color = F_Sp)) + 
+  geom_point() + 
+  facet_wrap(~Morphotype) + 
+  geom_smooth(method = "lm")
+
+
+
+Mod_L <- lm(L ~ Branch * Morphotype * F_Sp, data = dinfet)
+summary(Mod_L)
+
+
+
+Mod <- lm(log(Force) ~ Branch * Morphotype * F_Sp + Mussel_Weight, data = dinfet)
+
+anova(Mod)
+
+summary(Mod)
+
+
+
+`drop1(Mod, test = "F")
 
 
 Mod2 <- update(Mod, .~.- Branch:Morphotype:F_Sp)
--- Vitia_Ostrovsky/R_Calc_Crang_2024/crang_processing.R
+++ Vitia_Ostrovsky/R_Calc_Crang_2024/crang_processing.R
@@ -2,10 +2,58 @@ library(readxl)
 library(vegan)
 library(dplyr)
 library(ggplot2)
+library(patchwork)
+library(reshape2)
 
-Crang_2024 <- read_excel("Data/Crang_2024.xlsx", na = "NA")
+Crang_2024 <- read_excel("Data/Crang_2024.xlsx", na = "NA", sheet = "Full")
 
+
+# Создаем таблицу для сравнения двух рационов
+
+Crang_2024 %>% 
+  select(-c( Lon, Lat, Site,  Sample, ID, W, L_Car)) %>% 
+  melt(id.vars = "Area") %>% 
+  group_by(variable, Area) %>% 
+  summarise(Prop = round(mean(value), 3)) %>% 
+  dcast(variable ~ Area) -> Crangon_diet 
+  
+write.table(Crangon_diet, "clipboard", sep = "\t", row.names = F, dec = ",")
+
+names(Crangon_diet) <- c("Item", "Jouzh", "Sev")
+
+ggplot(Crangon_diet, aes(Jouzh, Sev)) +
+  geom_point() +
+  geom_abline()
+
+
+read_excel("Data/Crang_2024.xlsx", sheet = "Чистовые")%>% 
+  select(-c(ID, Site))%>% 
+  melt(id.vars = c("Area", "Cluster")) %>% 
+  group_by(Cluster, variable)%>%
+  summarise(N_mean = round(mean(value), 1)) %>% 
+  dcast(variable ~ Cluster) %>% 
+  write.table("clipboard", dec = ",", sep = "\t", row.names = F)
+
+read_excel("Data/Crang_2024.xlsx", sheet = "Full")%>% 
+  select(-c(Lon, Lat, Site,  Sample, ID, W, L_Car))%>% 
+  melt(id.vars = c("Area", "Cluster")) %>% 
+  group_by(Cluster, variable)%>%
+  summarise(N_mean = round(mean(value), 1)) %>% 
+  dcast(variable ~ Cluster) %>% 
+  write.table("clipboard", dec = ",", sep = "\t", row.names = F)
+
+  
+  
+
+
+
+
+  
 str(Crang_2024)
+
+
+
+
 
 
 
@@ -19,6 +67,8 @@ Crang_2024 %>%
 
 ord_crang <- metaMDS(crang[ ,-c(1:2) ], distance = "bray")
 
+
+
 plot(ord_crang, type = "t")
 
 
@@ -29,7 +79,12 @@ mds_points <- data.frame(scores(ord_crang)$sites)
 mds_points$Area <- crang$Area
 
 ggplot(mds_points, aes(NMDS1, NMDS2, color=Area)) +
-  geom_point(size = 4)
+  geom_point(size = 4)+
+  theme_bw()+
+  scale_color_manual(values = c("blue", "red"))+
+  labs(x = "MDS1", y = "MDS2", color = "Акватория")
+
+
 
 
 
@@ -52,8 +107,12 @@ plot(ord_comm, type = "t")
 mds_points_comm <- data.frame(scores(ord_comm)$sites)
 
 mds_points_comm$Area <-comm$Area
+
 ggplot(mds_points_comm, aes(  NMDS1, NMDS2,t, color = Area)) +
-  geom_point(size = 4)
+  geom_point(size = 4)+
+  theme_bw()+
+  scale_color_manual(values = c("blue", "red"))+
+labs(x = "MDS1", y = "MDS2", color = "Акватория")
 ################
 
 crang %>% 
@@ -83,13 +142,19 @@ comm %>%
   diverse$H_com
 
 ggplot(diverse, aes(x = H_com, y = Spec_Num))+
-  geom_point()+ 
-  geom_smooth(method = "lm")
+  geom_point(aes(color = Area), size = 4)+ 
+  geom_smooth(method = "lm") +
+  scale_color_manual(values = c("blue", "red")) +
+  labs(x = "Разнообразие в сообществе", y = "Количество видов в питании", color ="Акватория")+ 
+  theme_bw()
 
 
 ggplot(diverse, aes(x = H_com, y = Empty))+
-  geom_point()+ 
-  geom_smooth(method = "lm")
+  geom_point(aes(color = Area))+ 
+  geom_smooth(method = "lm")+ 
+theme_bw()+
+  scale_color_manual(values = c("blue", "red"))+
+  labs(x = "Разнообразие сообщества", y = "Пустые", color ="Акватория")  
 
 ggplot(diverse, aes(x = H_com, y = H_crang))+
   geom_point(aes(color = Area))+
@@ -113,5 +178,53 @@ ggplot(diverse, aes(x = comm_total_N, y = Spec_Num))+
 
 
 
-round(vegdist(comm[,-c(1:2)], method = "euclidean"), 0)
- 
+
+#########################
+comm_dist <- (vegdist(comm[,-c(1:2)], method = "bray"))
+
+dendr_comm <- as.dendrogram(hclust(d = comm_dist, method = "ward.D" ))
+
+plot(dendr_comm)
+p
+
+crang_dist <- (vegdist(crang[,-c(1:2)], method = "bray"))
+dendr_crang <- as.dendrogram(hclust(d = crang_dist, method = "ward.D" ))
+plot(dendr_crang)
+
+library(dendextend)
+
+
+
+tanglegram(dendr_comm, dendr_crang, sort = TRUE)
+
+
+untang_w <- untangle_step_rotate_2side(dendr_comm, dendr_crang, print_times = T)
+
+tanglegram(untang_w[[1]], untang_w[[2]],
+           highlight_distinct_edges = FALSE,
+           common_subtrees_color_lines = F,
+           main = "",
+           main_left = "Сообщество",
+           main_right = "Питание",
+           columns_width = c(8, 1, 8),
+           margin_top = 3.2, margin_bottom = 2.5,
+           margin_inner = 4, margin_outer = 0.5,
+           lwd = 1.2, edge.lwd = 1.2, 
+           lab.cex = 1.5, cex_main = 2)
+###########################
+# Размерно-весовая струткра
+
+ggplot(Crang_2024, aes(x = L_Car)) +
+  geom_histogram(aes(fill = Area)) +
+  facet_wrap(~Area, ncol = 1) +
+  scale_fill_manual(values = c("blue", "red")) +
+  theme_bw() +
+  labs(x = "Длина карапакса (мм)", y = "Частота", fill = "Акватория")
+
+
+ggplot(Crang_2024, aes(x = L_Car, y = W, color = Area)) +
+  geom_point() +
+  scale_color_manual(values = c("blue", "red"))+
+  theme_bw()+
+  labs(x = "Длина карапакса (мм)", y = "Частота", fill = "Акватория")
+
